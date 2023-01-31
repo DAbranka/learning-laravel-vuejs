066a068102dc639b2e4d8f57bbd5e80a8837a978
DAbranka <d.abranka@hotmail.com>
2023-01-31 12:13:20 +0100

    Navbar added with chirps link

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,6 +15,12 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+                    
+                    {{-- ! link to chirps --}}
+                    <x-nav-link :href="route('chirps.index')" :active="request()->routeIs('chirps.index')">
+                        {{ __('Chirps') }}
+                    </x-nav-link>
+
                 </div>
             </div>
 
@@ -69,6 +75,11 @@
         <div class="pt-2 pb-3 space-y-1">
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                 {{ __('Dashboard') }}
+            </x-responsive-nav-link>
+
+            {{-- ! adding link for chirp in responsive --}}
+            <x-responsive-nav-link :href="route('chirps.index')" :active="request()->routeIs('chirps.index')">
+                {{__('Chirps')}}
             </x-responsive-nav-link>
         </div>
 
