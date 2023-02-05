aeaa3d871ff4d9c2df42e7f4c90857b89f49f7a8
DAbranka <d.abranka@hotmail.com>
2023-02-05 20:46:37 +0100

    My Blog View & Layout Created

---
+++ resources/js/Layouts/MyBlogLayout.vue
@@ -0,0 +1,12 @@
+<script setup>
+    import MyBlog from '@/Pages/MyBlog.vue';
+</script>
+
+<template>
+    <title>My Blog</title>
+    <header>
+        <h1>My Blog</h1>
+    </header>
+
+    <slot />
+</template>
---
+++ resources/js/Pages/MyBlog.vue
@@ -0,0 +1,17 @@
+<script setup>
+import MyBlogLayout from "@/Layouts/MyBlogLayout.vue";
+</script>
+
+<template>
+    <MyBlogLayout>
+    <article class="container--flexbox">
+        <p class="container__paragraph">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quod sint
+            iure, sed recusandae optio voluptate? Ad, consectetur sint adipisci
+            debitis maxime voluptatum possimus obcaecati dolorum sequi ipsa
+            officiis repellat optio accusamus, itaque quos soluta minus
+            aspernatur cumque! Ut, repellendus quasi?
+        </p>
+    </article>
+</MyBlogLayout>
+</template>
