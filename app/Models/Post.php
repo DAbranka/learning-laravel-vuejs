3909a8e2b5270721d47bd0ad141eb2348f7b7574
DAbranka <d.abranka@hotmail.com>
2023-02-10 10:17:36 +0100

    Post DB - MRC Created & Updated

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,25 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 class Post extends Model
 {
     use HasFactory;
-
-    public static function all(){
-        return;
-    }
-
-    public static function find($slug){
-
-        if (!file_exists($path = resource_path("views/pages/posts/{$slug}.blade.php"))){
-            // return redirect('/posts');
-            // return abort(404);
-            throw new ModelNotFoundException;
-        }
-        
-        return cache()->remember("posts.{$slug}",1200, fn() => file_get_contents($path));
-    }
-
+    
 }
