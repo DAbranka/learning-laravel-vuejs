8a20daaf3f2c542bd31828473dc9ce87458bccfd
DAbranka <d.abranka@hotmail.com>
2023-02-10 10:57:40 +0100

    Post Migrations - Table Author added

--- database/migrations/2023_02_10_091303_create_posts_table.php
+++ database/migrations/2023_02_10_091303_create_posts_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->string('title');
             $table->text('excerpt');
             $table->string('body');
+            $table->string('author');
             $table->timestamps();
             $table->timestamp('published_at')->nullable();
         });
